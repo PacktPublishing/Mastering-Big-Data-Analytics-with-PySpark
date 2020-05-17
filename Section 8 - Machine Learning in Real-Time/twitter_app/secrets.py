ea0af32daee1a1a01182b593d528de59d9fbdf53
Danny Meijer <danny.meijer@nike.com>
2020-05-17 17:42:16 +0200

    update

--- Section 8 - Machine Learning in Real-Time/twitter_app/secrets.py
+++ Section 8 - Machine Learning in Real-Time/twitter_app/secrets.py
@@ -1,12 +1,12 @@
 """Replace the values below with your own Twitter API Keys, Secrets, and Tokens"""
 
 # Twitter Consumer API keys
-CONSUMER_KEY    = "<your_creds_go_here>"
-CONSUMER_SECRET = "<your_creds_go_here>"
+CONSUMER_KEY    = "szt9ytUAMAlEyqHiJH0c8hWas"
+CONSUMER_SECRET = "0m4A3izC3brsO0rto7C2A6Q52BgPVKxTRmbMJvJvdcHsedBVzY"
 
 # Twitter Access token & access token secret
-ACCESS_TOKEN    = "<your_creds_go_here>"
-ACCESS_SECRET   = "<your_creds_go_here>"
+ACCESS_TOKEN    = "1152234679065595906-Pat8sOnyeOi5onpSX7hM545GTc6UmE"
+ACCESS_SECRET   = "YEEw2viJr6URtYbnLw1Wqu5cjeMzMRvxkjwr4aku8TnUz"
 
 
 class TwitterSecrets:
